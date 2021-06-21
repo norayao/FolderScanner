ae945e20778f9ad88001d3c1d14d6a5a7c029e01
norayao <norayao0817@gmail.com>
2021-06-21 20:26:36 +0800

    remove translation functions

--- main.py
+++ main.py
@@ -4,19 +4,18 @@
 # @Link    :
 # @Version : Python3.7
 
+# import sys
+# import json
+# import requests
+# import random
+# import time
+# # google_trans_new 依赖于 json, requests, random, re
+# from google_trans_new import google_translator
+
 import os
-import sys
-import json
-import requests
-import random
 import re
-import time
 import tkinter as tk
 from tkinter import filedialog, Label
-
-# google_trans_new 依赖于 json, requests, random, re
-from google_trans_new import google_translator
-
 from openpyxl import Workbook
 
 
@@ -28,7 +27,7 @@ class Application(tk.Frame):
         self.filePath_entry = tk.Entry(self, width=30)
         self.master = master
         self.pack()
-        self.translator = google_translator()
+        # self.translator = google_translator()
         self.workbook = Workbook()
         self.worksheet = self.workbook.active
         self.create_widgets()
@@ -79,7 +78,7 @@ class Application(tk.Frame):
 
                 self.worksheet['A' + str(n)] = folderName
                 self.worksheet['B' + str(n)] = fileName
-                self.worksheet['C' + str(n)] = self.refactor(self.filename_translation(self.trim(fileName)))
+                self.worksheet['C' + str(n)] = '' # self.refactor(self.filename_translation(self.trim(fileName)))
                 self.worksheet['D' + str(n)] = fileSuffix
                 self.worksheet['E' + str(n)].value = filePath
                 self.worksheet['E' + str(n)].hyperlink = "file://" + filePath
@@ -89,29 +88,29 @@ class Application(tk.Frame):
                 self.folderLabel = tk.Label(self, text=folderLabelText, wraplength=350, justify='center')
                 self.folderLabel.grid(row=2, column=1, padx=5, pady=10)
             # 停止运行5秒防止谷歌拒绝服务
-            time.sleep(5)
+            # time.sleep(5)
 
 
         label_text = self.pathValue + "     目录已生成"
         self.result_lable = tk.Label(self, text=label_text, wraplength=350, justify='center')
         self.result_lable.grid(row=3, column=1, padx=5, pady=10)
 
-    def trim(self, text):
-        result = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", ",", text)
-        return result
-
-    def refactor(self, text):
-        # 此处的'，'为中文字符，Google translate自动转换了符号
-        result = text.replace('，', ' ')
-        return result
-
     def achieve_filelist(self, path):
         fileList = os.walk(path)
         return fileList
 
-    def filename_translation(self, text):
-        translate_text = self.translator.translate(self.trim(text), lang_tgt='zh')
-        return self.refactor(translate_text)
+    # def trim(self, text):
+    #     result = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", ",", text)
+    #     return result
+    #
+    # def refactor(self, text):
+    #     # 此处的'，'为中文字符，Google translate自动转换了符号
+    #     result = text.replace('，', ' ')
+    #     return result
+    #
+    # def filename_translation(self, text):
+    #     translate_text = self.translator.translate(self.trim(text), lang_tgt='zh')
+    #     return self.refactor(translate_text)
 
 
 if __name__ == "__main__":
